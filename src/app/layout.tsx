e2edc7d822359b5c08b7828654d6f53080c2caba
panadol <zaibejappa26@gmail.com>
2025-12-24 22:23:38 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import Navbar from "./components/Navbar";
 
 import "./globals.css"; //
 const Myfunction = () => {}
+export const metadata = {
+  title: "My Website",
+  viewport: "width=device-width, initial-scale=1",
+};
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
