bf3ddf04cd462b64d3302cfc6c3d188c9a4d356e
panadol <zaibejappa26@gmail.com>
2025-12-24 09:24:43 +0500

    update

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,24 +5,25 @@ import { Phone, Mail, MessageCircle } from 'lucide-react';
 export default function Contact() {
   return (
     <div style={{
-      padding: "50px 20px",
+      padding: "10px 20px",
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
-      gap: "25px",
-      backgroundColor: "#161739fd",
+      gap: "15px",
+      backgroundColor: "#17182efd",
       minHeight: "100vh",
       fontFamily: "Arial, sans-serif"
     }}>
       <h2 style={{ fontSize: "6rem", color: "#bae01eff", marginBottom: "10px" }}>Contact-Us</h2>
 
       <div style={{
-        backgroundColor: "white",
+        backgroundColor: "rgba(11, 53, 142, 1)",
         padding: "30px",
-        borderRadius: "15px",
-        boxShadow: "0 4px 15px rgba(0,0,0,0.1)",
+        borderRadius: "55px",
+        boxShadow: "0 16px 15px rgba(171, 171, 46, 0.1)",
         width: "100%",
-        maxWidth: "400px",
+        minHeight: "10vh",
+        maxWidth: "1000px",
         display: "flex",
         flexDirection: "column",
         gap: "20px"
@@ -30,20 +31,24 @@ export default function Contact() {
         
         {/* Phone Section */}
         <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
-          <Phone color="#2563eb" size={24} />
+          <Phone color="#0b3285ff" size={44} />
           <div>
-            <p style={{ margin: 0, fontWeight: "bold", color: "#555" }}>Call Us</p>
-            <p style={{ fontSize: "2rem", margin: 0,color: "#333" }}>0300-2012466</p>
-            <p style={{ fontSize: "2rem", margin: 0,color: "#333" }}>0345-2012466</p>
+            <p style={{ fontSize: "2.0rem", margin: 0, fontWeight: "bold", color: "#e21b46ff" }}>Call Us</p>
+            <p style={{ fontSize: "2rem", margin: 0,color: "white" }}>0300-2012466</p>
+            <p style={{ fontSize: "2rem", margin: 0,color: "white" }}>0345-2012466</p>
           </div>
         </div>
 
         {/* Email Section */}
         <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
-          <Mail color="#ea4335" size={24} />
+          <Mail color="#ea4335" size={44} />
           <div>
-            <p style={{ margin: 0, fontWeight: "bold", color: "#e21b46ff" }}>Email Address</p>
-            <p style={{ fontSize: "1rem", margin: 0,color: "#333" }}>zaibejappa26@gmail.com</p>
+            <p style={{ fontSize: "2.0rem", margin: 0, fontWeight: "bold", color: "#e21b46ff" }}>Email Adress</p>
+            <p style={{ fontSize: "1.6rem", margin: 0,color: "white" }}>marhabaproperties.pk@gmail.com</p>
+        
+             <p style={{ fontSize: "2.0rem", margin: 0, fontWeight: "bold", color: "#e21b46ff" }}>Address</p>
+            <p style={{ fontSize: "1.6rem", margin: 0,color: "white" }}>Thatha Moosa, Zero Point, Bhowana , 
+              Tehsil Bhowana, District Chiniot.</p>
           </div>
         </div>
 
@@ -57,17 +62,17 @@ export default function Contact() {
             alignItems: "center",
             justifyContent: "center",
             gap: "10px",
-            backgroundColor: "#25D366",
+            backgroundColor: "#0d913dff",
             color: "white",
             padding: "12px",
             borderRadius: "8px",
             textDecoration: "none",
             fontWeight: "bold",
             marginTop: "10px",
-            transition: "0.3s"
+            transition: "1.3s"
           }}
         >
-          <MessageCircle size={24} />
+          <MessageCircle size={54} />
           WhatsApp Now
         </a>
 
