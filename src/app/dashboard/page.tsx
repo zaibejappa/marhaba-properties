cb8b5f2e99cc77f94f05135ec1daa11071c70078
panadol <zaibejappa26@gmail.com>
2025-12-25 02:40:16 +0500

    update

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+        export default function Dashboard() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+      <p>Yahan admin properties add / delete kare ga</p>
+    </div>
+  );
+}
+  
---
+++ src/app/login/page.tsx
@@ -0,0 +1,62 @@
+
+        "use client";
+
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/app/components/lib/firebase";
+import { useRouter } from "next/navigation";
+
+const ADMIN_EMAIL = "admin@gmail.com";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password);
+
+      if (res.user.email === ADMIN_EMAIL) {
+        router.push("/dashboard"); // 👑 admin
+      } else {
+        router.push("/properties"); // 👥 public user
+      }
+    } catch (err: any) {
+      setError("Email ya password ghalat hai");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow w-full max-w-md">
+        <h2 className="text-2xl font-bold mb-4 text-center">Login</h2>
+
+        <input
+          className="w-full border p-2 mb-3"
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          className="w-full border p-2 mb-3"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {error && <p className="text-red-500 mb-2">{error}</p>}
+
+        <button
+          onClick={handleLogin}
+          className="w-full bg-blue-600 text-white py-2 rounded"
+        >
+          Login
+        </button>
+      </div>
+    </div>
+  );
+}
+   
